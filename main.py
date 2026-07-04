d37839ce2198b54fba68fd071d6f9c306e6d3c37
md-danish-lg <dk665226@gmail.com>
2026-07-04 18:59:48 +0500

    Add initial project setup with Dockerfile, environment configuration, and FastAPI endpoints

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from groq import Groq
+from dotenv import load_dotenv
+import chromadb
+
+load_dotenv()
+
+
+app = FastAPI()
+llm_client = Groq()
+chroma_client = chromadb.Client()
+collection = chroma_client.get_or_create_collection("vehicle_collection")
+
+
+
+
+class RepairHistory(BaseModel):
+    id: str = Field(min_length=1)
+    text: str = Field(min_length=1)
+
+
+class RepairHistoryQuery(BaseModel):
+    text: str = Field(min_length=1)
+    result_length: int = 3
+
+@app.get("/")
+async def hello_world():
+    return {"text":"hello world"}
+
+
+@app.get("/health")
+async def app_status():
+    return {"status" : "ok"}
+
+
+@app.post("/repair-history/add")
+async def add_repair_history(repair: RepairHistory):
+    collection.add(
+        documents=[repair.text], 
+        ids=[repair.id]
+    )
+    return {"status": "record created successfully"}
+
+
+@app.post("/repair-history/search")
+async def search_repair_history(repair: RepairHistoryQuery):    
+    result = collection.query(query_texts=[repair.text], n_results=repair.result_length)
+    return {"result" : result['documents'][0]}
+
